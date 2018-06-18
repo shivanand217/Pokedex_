82584cec5f4807c382f3732efc39a35083b6118b
shiv anand <shivakp2111@gmail.com>
2018-06-18 23:53:34 +0530

    parsing our JSON api data

--- pokedex/Model/Pokemon.swift
+++ pokedex/Model/Pokemon.swift
@@ -5,7 +5,6 @@
 //  Created by apple on 15/06/18.
 //  Copyright © 2018 shiv. All rights reserved.
 //
-
 import Foundation
 import Alamofire
 
@@ -15,12 +14,14 @@ class Pokemon {
     private var _pokedexId: Int!
     private var _description: String!
     private var _type: String!
-    private var _defense: String!
-    private var _height: String!
-    private var _weight: String!
-    private var _attack: String!
+    private var _defense: Int!
+    private var _height: Int!
+    private var _weight: Int!
+    private var _attack: Int!
     private var _nextEvolutionTxt: String!
     private var _pokemonURL: String!
+    private var _speed: Int!
+    private var _baseExperience: Int!
     
     var name: String {
         return _name
@@ -33,14 +34,48 @@ class Pokemon {
     init(name: String, pokedexId: Int) {
         self._name = name
         self._pokedexId = pokedexId
-        self._pokemonURL = "\(URL_BASE)\(URL_POKEMON)\(self.pokedexId)/"
+        self._pokemonURL = "\(URL_BASE)\(URL_POKEMON)\(pokedexId)/"
     }
     
     func downloadPokemonDetail(completed: DownloadCompleted) {
+        
         // request the api to get the data of the specific pokemon
         Alamofire.request(_pokemonURL, method: .get).responseJSON { (response) in
-            print(response.result.value)
+            //print(response.result.value)
+            
+            if let dict = response.result.value as? Dictionary<String, AnyObject> {
+                
+                if let weight = dict["weight"] as? Int {
+                    self._weight = weight
+                }
+                if let height = dict["height"] as? Int {
+                    self._height = height
+                }
+                if let name = dict["name"] as? String {
+                    self._name = name
+                }
+                if let pokedexId = dict["id"] as? Int {
+                    self._pokedexId = pokedexId
+                }
+                if let baseExp = dict["base_experience"] as? Int {
+                    self._baseExperience = baseExp
+                }
+            }
+            
+            if let dict1 = response.result.value as? Dictionary<String, Dictionary<String, AnyObject> > {
+                if let slot = dict1["types"]!["slot"] {
+                    print(slot)
+                }
+            }
+            
+            /* dict = response.result.value as! Dictionary<String, AnyObject>; do {
+                
+                if let weight = dict["weight"] as? String {
+                    self._weight = weight
+                }
+            }*/
         }
     }
+    
     
 }
--- pokedex/Utils/Constants.swift
+++ pokedex/Utils/Constants.swift
@@ -8,8 +8,8 @@
 
 import Foundation
 
-let URL_BASE = "http://pokeapi.co"
-let URL_POKEMON = "/api/v1/pokemon/"
+let URL_BASE = "http://pokeapi.co/api/v2/"
+let URL_POKEMON = "pokemon/"
 
 
 typealias DownloadCompleted = () -> ()
