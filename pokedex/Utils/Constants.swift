e1be65708b4a2c873b23f610f5694732b4371f35
shiv anand <shivakp2111@gmail.com>
2018-06-18 10:48:00 +0530

    store api URLs

---
+++ pokedex/Utils/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  pokedex
+//
+//  Created by apple on 18/06/18.
+//  Copyright © 2018 shiv. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "http://pokeapi.co"
+let URL_POKEMON = "/api/v1/pokemon/"
